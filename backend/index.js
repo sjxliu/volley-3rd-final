8944ac4d1e199fbe6c7aa31a9b18be9c2032df58
unknown <sjxliu8@gmail.com>
2022-04-27 22:20:57 -0500

    working on post

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,11 @@ const app = express();
 
 
 //middleware
-app.use('/posts', postRoutes)
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 app.use(cors())
+app.use('/posts', postRoutes)
+
 
 
 //mongodb
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Paper, Typography } from "@material-ui/core";
 
 import useStyles from "./FormStyles";
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    caption: "",
+    tags: "",
+    selectedFile: "",
+  });
   const dazzle_it = useStyles();
-  return <h1>FORM</h1>;
+
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={dazzle_it.base}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={dazzle_it.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Spike It!</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
