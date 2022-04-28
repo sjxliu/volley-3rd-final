fabee975b48f7a99bc35011435d77b4d98e283d0
unknown <sjxliu8@gmail.com>
2022-04-27 19:11:53 -0500

    home screen in prog

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,42 @@
+// Dependency imports
 import React from "react";
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-import volley_img from './images/volley_logo.png'
-
+// Variable imports
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import volley_img from "./images/volley-logo.png";
+import useStyles from "./styles";
 
 const App = () => {
+  const dazzle_it = useStyles();
+
   return (
-   <Container maxwidth='lg'>
-<AppBar position="static" color="inherit">
-<Typography variant="h2" align="center">
-Volley
-</Typography>
-<img src={volley_img} alt='volley_logo' height='60' /> 
-</AppBar>
-   </Container>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit" className={dazzle_it.appBar}>
+        {/* <Typography variant="h2" align="center" className={dazzle_it.heading}>
+          Volley
+        </Typography> */}
+        <img src={volley_img} alt="volley_logo" height="150" className={dazzle_it.image}/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            spacing={3}
+            justifyContent="space-between"
+            alignItems="stretch"
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  heading: {
+    color: "rbga(2, 68, 66)",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
